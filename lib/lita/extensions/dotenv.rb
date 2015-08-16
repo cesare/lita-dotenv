140c74856d3db258077968374fd03f19adca773a
SAWADA Tadashi <cesare@mayverse.jp>
2015-08-16 17:40:53 +0900

    Load dotenv on starting up

--- lib/lita/extensions/dotenv.rb
+++ lib/lita/extensions/dotenv.rb
@@ -1,6 +1,15 @@
+require 'dotenv'
+
 module Lita
   module Extensions
     class Dotenv
+      class << self
+        def call(payload)
+          ::Dotenv.load
+        end
+      end
     end
+
+    Lita.register_hook(:before_run, Dotenv)
   end
 end
